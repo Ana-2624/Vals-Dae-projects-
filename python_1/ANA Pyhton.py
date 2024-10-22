ee6ed09c51fb166179b64a3ddf0178b2da6710e4
Ana-2624 <anitamoralestepale08@gmail.com>
2024-10-22 17:50:07 -0400

    Added new functions

---
+++ python_1/ANA Pyhton.py
@@ -0,0 +1,35 @@
+def myfunction():
+    print('hello world')
+
+myfunction()
+myfunction()
+myfunction()
+
+# of logos 
+numbeer_of_times=5
+
+for index in range(10):
+    print("hello Ana")
+
+    #condition 
+
+
+    cond=1 
+
+
+while cond <5: 
+        print ("hello pinky")
+        cond= cond+1
+        print(cond)
+
+def main():
+    try:
+        userName = input("What is your name:")
+        userName = userName + 1 
+    except Exception as our Exception:
+        print("Oooopsan error occured",ourException)
+     else:
+        print(userName)
+     finally:
+        print( "Program done ")
+
